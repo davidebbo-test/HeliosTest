a81974f9116e0a966f90aeeba1f23c6f782086ed
davidebbo <david.ebbo@microsoft.com>
2014-02-19 21:59:52 -0800

    Initial commit

---
+++ HeliosTest/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace HeliosTest
+{
+    using System;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // New code:
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
