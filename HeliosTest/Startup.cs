80d73325a641af4e24e003b18912ec2117dec47f
davidebbo <david.ebbo@microsoft.com>
2014-02-19 22:00:52 -0800

    Show loaded assemblies

--- HeliosTest/Startup.cs
+++ HeliosTest/Startup.cs
@@ -11,7 +11,16 @@
             app.Run(async context =>
             {
                 context.Response.ContentType = "text/plain";
-                await context.Response.WriteAsync("Hello, world.");
+
+                await context.Response.WriteAsync("Assemblies in AppDomain:" + Environment.NewLine);
+                foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    await context.Response.WriteAsync(asm.FullName + Environment.NewLine);
+                }
+                await context.Response.WriteAsync(Environment.NewLine);
+
+                await context.Response.WriteAsync("Stack trace:" + Environment.NewLine);
+                await context.Response.WriteAsync(Environment.StackTrace);
             });
         }
     }
